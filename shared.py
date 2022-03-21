ec0d4a0192f2479eab04fcd16fb6f798ae07712d
omari <mmomari072@gmail.com>
2022-03-21 16:29:21 +0300

    1st time

---
+++ shared.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 14:39:37 2022
+
+@author: omari
+"""
+import thingspeak
+ch = thingspeak.Channel(9)
+import pickle
+fp = open("shared.pkl","rb")
+from time import sleep
+Before = None
+Before_tmp = None
+while True:
+    try:
+        shared = pickle.load(fp)
+        Before_tmp = shared["r_id"]
+    except:
+        pass
+    if Before == Before_tmp:
+        sleep(1);
+        continue
+    Before= Before_tmp
+    print (shared)
+    sleep(1)
+
